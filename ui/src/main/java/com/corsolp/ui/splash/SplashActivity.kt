66a7eae6c7b52100b505cc71aaac9dcaef3ffd2d
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2025-05-15 17:17:32 +0200

    Create: splash activity

---
+++ ui/src/main/java/com/corsolp/ui/splash/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.corsolp.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.corsolp.ui.MainActivity
+import com.corsolp.ui.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
