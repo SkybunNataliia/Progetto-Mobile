8641b2746c57aa0d1690a1aad427c5bf0279497a
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2025-06-02 22:39:05 +0200

    Add: mappers in weather repository impl

---
+++ data/src/main/java/com/corsolp/data/repository/WeatherRepositoryImpl.kt
@@ -0,0 +1,75 @@
+package com.corsolp.data.repository
+
+import android.content.Context
+import com.corsolp.data.remote.WeatherApi
+import com.corsolp.data.remote.models.ForecastRemoteModel
+import com.corsolp.data.remote.models.GeoLocationRemoteModel
+import com.corsolp.data.remote.models.WeatherRemoteModel
+import com.corsolp.domain.models.City
+import com.corsolp.domain.models.Forecast
+import com.corsolp.domain.models.ForecastItem
+import com.corsolp.domain.models.Weather
+import com.corsolp.domain.repository.WeatherRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+class WeatherRepositoryImpl (
+    private val context: Context,
+    private val weatherApi: WeatherApi
+) : WeatherRepository {
+
+    private val scope = CoroutineScope(Dispatchers.IO)
+
+    override fun fetchFavorites(): Flow<Set<City>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun addFavoriteCity(city: City): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun removeFavoriteCity(city: City): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun fetchCurrentWeather(city: City): Weather {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun fetchForecast(city: City): Forecast {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun geocodeCity(cityName: String): City? {
+        val response = weatherApi.geocodeCity(city = cityName)
+        return response.firstOrNull()?.toDomain()
+    }
+
+    private fun GeoLocationRemoteModel.toDomain(): City = City(
+        name = this.name,
+        country = this.country,
+        lat = this.lat,
+        lon = this.lon,
+    )
+
+    private fun WeatherRemoteModel.toDomain(): Weather = Weather(
+        name = this.weather.firstOrNull()?.name ?: "",
+        temperature = this.main.temp.toDouble(),
+        description = this.weather.firstOrNull()?.description ?: ""
+    )
+
+    private fun ForecastRemoteModel.toDomain(): Forecast = Forecast(
+        items = this.list.map {
+            ForecastItem(
+                dateTime = it.date ?: "",
+                weather = it.weather.firstOrNull()?.name ?: "",
+                temperature = it.main.temp.toDouble(),
+                tempMin = it.main.tempMin.toDouble(),
+                tempMax = it.main.tempMax.toDouble(),
+                description = it.weather.firstOrNull()?.description ?: ""
+            )
+        }
+    )
+}
